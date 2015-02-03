57c0df2330f3ed121e2a423b365648c38c0a5bb0
psirenny <djtorres0@gmail.com>
2015-02-02 23:21:19 -0500

    bunch of fixes

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+var _ = require('lodash');
+
 function Component() {}
 
 Component.prototype.view = __dirname;
 
-Component.prototype.init = function () {
-  // do nothing
+Component.prototype.init = function (model) {
+  var self = this;
+
+  model.start('index', 'items', 'value', function (items, value) {
+    return _.findIndex(items, {value: value});
+  });
+
+  model.start('selected', 'items', 'index', function (items, index) {
+    return items[index];
+  });
+
+  model.setNull('defaultValue', model.get('value'));
+
+  model.start('value', '_value', 'defaultValue', {
+    get: function (_value, defaultValue) {
+      return _value || defaultValue;
+    },
+    set: function (value, _value, defaultValue) {
+      return [value, defaultValue];
+    }
+  });
 };
 
 Component.prototype.create = function (model, dom) {
@@ -20,19 +41,22 @@ Component.prototype.create = function (model, dom) {
 
 Component.prototype.open = function (e) {
   e.stopPropagation();
+  if (this.model.get('disabled')) return;
   this.model.set('open', true);
   this.emit('open');
 };
 
 Component.prototype.close = function (e) {
+  if (this.model.get('disabled')) return;
   this.model.del('open');
   this.emit('close');
 };
 
-Component.prototype.select = function (e, item) {
+Component.prototype.select = function (e, index, item) {
   e.stopPropagation();
+  if (this.model.get('disabled')) return;
   this.model.set('value', item.value);
-  this.emit('select', item);
+  this.emit('select', index, item);
   this.close();
 };
 
